b6257ae0e99cc3f10133b7bb0c7e10453c55d02b
Markus Ewald <cygon@nuclex.org>
2007-05-11 21:23:35 +0000

    Added empty class for planned TrackingBar control; copied CPL statement in front of all source files
    git-svn-id: file:///srv/devel/repo-conversion/nuwi@4 d2e56fa2-650e-0410-a79f-9358c0239efd

---
+++ Source/TrackingBar/TrackingBar.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nuclex.Windows.Forms {
+
+  /// <summary>Progress bar for tracking the progress of background operations</summary>
+  public partial class TrackingBar : ProgressBar {
+
+    /// <summary>Initializes a new tracking bar</summary>
+    public TrackingBar() {
+      InitializeComponent();
+    }
+
+  }
+
+} // namespace Nuclex.Windows.Forms
