1bb2363a07d0ff7bf7bfdee0f3312063544461d3
cygon <cygon@nuclex.org>
2024-06-14 16:42:33 +0200

    Changed license to Apache License 2.0

--- Source/Views/ViewControl.cs
+++ Source/Views/ViewControl.cs
@@ -1,22 +1,21 @@
-﻿#region CPL License
+#region Apache License 2.0
 /*
-Nuclex Framework
-Copyright (C) 2002-2019 Nuclex Development Labs
+Nuclex .NET Framework
+Copyright (C) 2002-2024 Markus Ewald / Nuclex Development Labs
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the IBM Common Public License as
-published by the IBM Corporation; either version 1.0 of the
-License, or (at your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-IBM Common Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the IBM Common Public
-License along with this library
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
-#endregion
+#endregion // Apache License 2.0
 
 using System;
 using System.ComponentModel;
